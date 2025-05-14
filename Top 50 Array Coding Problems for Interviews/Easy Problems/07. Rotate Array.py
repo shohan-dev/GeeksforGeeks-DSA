5a5965257bbf08f2a89ef3068db83501d16a4f19
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-14 23:27:43 +0600

    problem  7

--- Top 50 Array Coding Problems for Interviews/Easy Problems/07. Rotate Array.py
+++ Top 50 Array Coding Problems for Interviews/Easy Problems/07. Rotate Array.py
@@ -1,2 +1,6 @@
-# Rotate Array
-
+class Solution:
+    def rotateArr(self, arr, k):
+        k = k % len(arr)
+        rotated = arr[k:] + arr[:k]
+        for i in range(len(arr)):
+            arr[i] = rotated[i]  # write back into arr only for the main arr to update
