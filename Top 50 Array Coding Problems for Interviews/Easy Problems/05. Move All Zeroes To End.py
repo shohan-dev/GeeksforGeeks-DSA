bb3f7d9ea6640398c91fa2b3b2ff469036f93a85
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-14 23:24:18 +0600

    problem 5

--- Top 50 Array Coding Problems for Interviews/Easy Problems/05. Move All Zeroes To End.py
+++ Top 50 Array Coding Problems for Interviews/Easy Problems/05. Move All Zeroes To End.py
@@ -1,2 +1,14 @@
-# Move All Zeroes To End
+class Solution:
+    def pushZerosToEnd(self, arr):
+        n = len(arr)
+        pos =0
+        
+        for i in range(n):
+            if arr[i] != 0:
+                arr[pos] = arr[i]
+                pos+=1
+        while pos<n:
+            arr[pos] = 0
+            pos+=1
+        return arr
 
