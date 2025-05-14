beb4e5f66a903f2a1220805e63a733cb81683a1c
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-14 23:28:00 +0600

    problem  9

--- Top 50 Array Coding Problems for Interviews/Easy Problems/09. Plus One.py
+++ Top 50 Array Coding Problems for Interviews/Easy Problems/09. Plus One.py
@@ -1,2 +1,13 @@
-# Plus One
+class Solution:
 
+    def addOne(self, arr):
+        n = len(arr)
+        for i in range(n - 1, -1, -1):
+
+            if arr[i] < 9:
+                arr[i] += 1
+                return arr
+
+            arr[i] = 0
+            
+        return [1] + [0] * n
