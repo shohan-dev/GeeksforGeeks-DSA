f4dc8a0b97fd873be9bac5ab83096e153c24c489
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-14 23:22:56 +0600

    probelm 3

--- Top 50 Array Coding Problems for Interviews/Easy Problems/03. Three Great Candidates.py
+++ Top 50 Array Coding Problems for Interviews/Easy Problems/03. Three Great Candidates.py
@@ -1,2 +1,8 @@
-# Three Great Candidates
-
+class Solution:
+    def maxProduct(self, arr):
+        arr.sort()
+        # Case 1: Product of top 3 max elements
+        case1 = arr[-1] * arr[-2] * arr[-3]
+        # Case 2: Product of 2 smallest (most negative) and the largest
+        case2 = arr[0] * arr[1] * arr[-1]
+        return max(case1, case2)  # Return the maximum of both cases 
