ce3e4685f06a80cfd915431a5a1ce3e5dbe5879c
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-14 23:27:50 +0600

    problem  8

--- Top 50 Array Coding Problems for Interviews/Easy Problems/08. Wave Array.py
+++ Top 50 Array Coding Problems for Interviews/Easy Problems/08. Wave Array.py
@@ -1,2 +1,9 @@
-# Wave Array
+from typing import List
+class Solution:
+    def convertToWave(self, arr : List[int]) -> None:
+        # code here
+        arr.sort()
 
+        for i in range(0, len(arr)-1,2):
+            arr[i], arr[i+1] = arr[i+1] , arr[i]
+        return arr
