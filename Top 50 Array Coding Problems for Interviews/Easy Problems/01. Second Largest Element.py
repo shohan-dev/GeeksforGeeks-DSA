5bd1019a6046fcce7d2094e6f5500ebffd584c20
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-14 23:21:14 +0600

    problem 1

--- Top 50 Array Coding Problems for Interviews/Easy Problems/01. Second Largest Element.py
+++ Top 50 Array Coding Problems for Interviews/Easy Problems/01. Second Largest Element.py
@@ -1,2 +1,9 @@
-# Second Largest Element
+class Solution:
+    def getSecondLargest(self, a):
+        a = set(a)           # remove duplicates
+        if len(a) < 2:       # less than 2 unique elements
+            return -1
+        a = list(a)          # convert set to list
+        a.sort()
+        return a[-2]         # second largest
 
