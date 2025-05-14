0bf9fb5374fb9e84480d316bae53b25277e16932
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-14 23:23:55 +0600

    probelm 4

--- Top 50 Array Coding Problems for Interviews/Easy Problems/04. Max Consecutive Ones.py
+++ Top 50 Array Coding Problems for Interviews/Easy Problems/04. Max Consecutive Ones.py
@@ -1,2 +1,11 @@
-# Max Consecutive Ones
-
+class Solution:
+    def maxConsecutiveCount(self, arr):
+        #code here 
+        one =0
+        zero =0
+        for i in arr:
+            if i==1:
+               one+=1
+            else:
+                zero+=1
+        return max(one,zero)
