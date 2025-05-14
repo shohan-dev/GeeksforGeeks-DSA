06529a7fd14151ff7d734ef2b8908e3edbc740bf
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-14 23:27:19 +0600

    problem 6

--- Top 50 Array Coding Problems for Interviews/Easy Problems/06. Reverse Array in Groups.py
+++ Top 50 Array Coding Problems for Interviews/Easy Problems/06. Reverse Array in Groups.py
@@ -1,2 +1,5 @@
-# Reverse Array in Groups
-
+class Solution:
+    def reverseInGroups(self, arr, k):
+        for i in range(0,len(arr)-1,k):
+            arr[i:i+k] = reversed(arr[i:i+k])
+        return arr
