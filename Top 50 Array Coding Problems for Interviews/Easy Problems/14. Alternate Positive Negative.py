8622a4508eb4c8187203280b577a712e9264b18f
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-15 23:53:39 +0600

    problem  14

--- Top 50 Array Coding Problems for Interviews/Easy Problems/14. Alternate Positive Negative.py
+++ Top 50 Array Coding Problems for Interviews/Easy Problems/14. Alternate Positive Negative.py
@@ -1,2 +1,27 @@
-# Alternate Positive Negative
+class Solution:
+    def rearrange(self,arr):
+        pos , neg = [],[]
 
+        for i in arr:
+            if i>=0:
+                pos.append(i)
+            else:
+                neg.append(i)
+        
+        # pos,neg
+        p =0
+        s = min(len(pos),len(neg))
+        m = max(len(pos),len(neg))
+        
+        final =[]
+        for i in range(s):
+            final.append(pos[i])
+            final.append(neg[i])
+            p +=1
+        
+        final.extend(pos[p:])
+        final.extend(neg[p:])
+        
+        for i in range(len(final)):
+            arr[i] = final[i]
+        return arr  
