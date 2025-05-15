131017d4fc1928325bb33254f33772fcd00b6a8b
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-15 23:53:08 +0600

    problem 10

--- Top 50 Array Coding Problems for Interviews/Easy Problems/10. Stock Buy and Sell – One Transaction.py
+++ Top 50 Array Coding Problems for Interviews/Easy Problems/10. Stock Buy and Sell – One Transaction.py
@@ -1,2 +1,12 @@
-# Stock Buy and Sell – One Transaction
-
+class Solution:
+    def maximumProfit(self, arr):
+        
+        min_price = arr[0]
+        max_profit = 0
+        
+        for i in range(1,len(arr)):
+            cost = arr[i] - min_price
+            max_profit = max(max_profit,cost)
+            min_price = min(min_price,arr[i])
+        
+        return max_profit
