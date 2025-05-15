09df8f718ccb91cf057fdfbe630b40e6bad80e6f
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-15 23:55:00 +0600

    problem 17

--- Top 50 Array Coding Problems for Interviews/Easy Problems/18. Missing Ranges of Numbers.py
+++ Top 50 Array Coding Problems for Interviews/Easy Problems/18. Missing Ranges of Numbers.py
@@ -1,2 +1,15 @@
-# Missing Ranges of Numbers
+class Solution:
+    def missingRanges(self, a, lower, upper):
+        final = []
+    
+        if a[0] > lower:
+            final.append([lower, a[0] - 1])
+        
+        for i in range(len(a) - 1):
+            if a[i+1] - a[i] > 1:
+                final.append([a[i] + 1, a[i+1] - 1])
+        
+        if a[-1] < upper:
+            final.append([a[-1] + 1, upper])
 
+        return final
