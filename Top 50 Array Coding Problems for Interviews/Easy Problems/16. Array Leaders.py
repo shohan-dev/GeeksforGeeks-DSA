a03220c7531ea42054e36726be480fe7d407d2b4
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-15 23:53:54 +0600

    16

--- Top 50 Array Coding Problems for Interviews/Easy Problems/16. Array Leaders.py
+++ Top 50 Array Coding Problems for Interviews/Easy Problems/16. Array Leaders.py
@@ -1,2 +1,11 @@
-# Array Leaders
-
+class Solution:
+    def leaders(self, a):
+        max_val = -1
+        final = []
+        for i in range(len(a)-1,-1,-1):
+            if max_val <= a[i]:
+                final.append(a[i])
+                max_val = a[i]
+        
+        final.reverse()
+        return final
