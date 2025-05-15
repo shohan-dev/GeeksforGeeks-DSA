bdbca646c6470e8ef9b6986cbfdb238f1b3d357d
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-15 23:53:21 +0600

    problem  12

--- Top 50 Array Coding Problems for Interviews/Easy Problems/12. Remove All Occurrences of Element.py
+++ Top 50 Array Coding Problems for Interviews/Easy Problems/12. Remove All Occurrences of Element.py
@@ -1,2 +1,14 @@
-# Remove All Occurrences of Element
-
+def removeElement(arr, ele):
+  
+    count =0
+    for i in arr:
+        if i==ele:
+            continue
+        count +=1          
+                
+    return count
+  
+if __name__ == "__main__":
+    arr = [0, 1, 3, 0, 2, 2, 4, 2]
+    ele = 2
+    print(removeElement(arr, ele))
