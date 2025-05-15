10745a0d5dac6f9753e8c00c2c3357b631aceab7
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-05-15 23:54:54 +0600

    problem 17

--- Top 50 Array Coding Problems for Interviews/Easy Problems/17. Missing and Repeating in Array.py
+++ Top 50 Array Coding Problems for Interviews/Easy Problems/17. Missing and Repeating in Array.py
@@ -1,2 +1,22 @@
-# Missing and Repeating in Array
-
+class Solution:
+    def findTwoElement( self,a): 
+        s = set()
+        missing =0
+        duplicate = 0
+        
+        for i in a:
+            if i in s:
+                duplicate = i
+                
+            else:
+                s.add(i)
+        
+        for i in range(1,max(a)+1):
+            if i in s:
+                continue
+            else:
+                missing = i
+        if missing==0:
+            missing = max(a)+1
+        
+        return [duplicate,missing]
